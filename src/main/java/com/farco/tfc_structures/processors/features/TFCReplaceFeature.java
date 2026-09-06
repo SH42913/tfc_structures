29508c10891ef4d2dfc4059c808518180e7e583b
SH42913 <shoot913@gmail.com>
2026-09-06 12:17:36 +0400

    Mycelium will be replaced as Rooted Dirt

--- src/main/java/com/farco/tfc_structures/processors/features/TFCReplaceFeature.java
+++ src/main/java/com/farco/tfc_structures/processors/features/TFCReplaceFeature.java
@@ -137,7 +137,8 @@ public class TFCReplaceFeature implements ReplaceFeature {
                 new Pair<>(Blocks.MUD, SoilBlockType.MUD),
                 new Pair<>(Blocks.MUD_BRICKS, SoilBlockType.MUD_BRICKS),
                 new Pair<>(Blocks.MUDDY_MANGROVE_ROOTS, SoilBlockType.MUDDY_ROOTS),
-                new Pair<>(Blocks.PODZOL, SoilBlockType.ROOTED_DIRT)
+                new Pair<>(Blocks.PODZOL, SoilBlockType.ROOTED_DIRT),
+                new Pair<>(Blocks.MYCELIUM, SoilBlockType.ROOTED_DIRT)
         );
 
         tagToOreMappings = List.of(
